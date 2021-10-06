bc69734e78af0cecdc6192640f5ebfe8012eaea8
nammyeon <91935298+nammyeon@users.noreply.github.com>
2021-10-06 13:13:34 +0900

    Add files via upload

---
+++ 20211006/js/training001.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".김남").on("click",function(){
+        $("h1").hide();
+    });
+
+    $("#오징어").on("click",function(){
+        $("h1").show();
+    });
+
+    $(".나는버튼").on("click",function(){
+        $("figure").toggleClass("on")
+    });
+});
