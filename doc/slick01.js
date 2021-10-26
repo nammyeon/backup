d0c4901cd68b6edf537a4fccd023fa8adcc322be
nammyeon <91935298+nammyeon@users.noreply.github.com>
2021-10-26 13:57:12 +0900

    Add files via upload

---
+++ doc/slick01.js
@@ -0,0 +1,14 @@
+$ (function(){
+   //-------------------
+   $('.main_slider').slick({
+       arrows:false,
+       autoplay:true,
+       pauseOnHover:false,
+       //vertical:true, //아래위로 슬라이드
+       fade:true, //페이드되며 슬라이드
+     
+   });
+   
+
+   //-------------------
+})
---
+++ doc/slick02.js
@@ -0,0 +1,32 @@
+$ (function(){
+   //-------------------
+
+
+   //메인슬라이드
+   $('.main_slider').slick({
+       arrows:false,
+       autoplay:true,
+       pauseOnHover:false,
+       
+     
+   });
+   
+
+
+
+   //제품슬라이드
+   $('.pr_slider').slick({
+         arrows:false,
+         slidesToShow: 3,
+         autoplay:true,
+         
+         dots:true,
+       
+   })
+
+
+
+
+
+   //-------------------
+})
