43defa6d27786126784637e56052d3f2786c75c9
nammyeon <91935298+nammyeon@users.noreply.github.com>
2021-11-03 12:54:06 +0900

    Add files via upload

---
+++ doc/basic05.js
@@ -0,0 +1,17 @@
+$(function(){
+//---------------------------------
+$('#top_banner i').on('click', function(){
+    // $('#top_banner').hide();
+    $('#top_banner').slideUp();
+});
+
+
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:2000,    
+    pauseOnFocus:false,
+    pauseOnHover:false,
+});
+//---------------------------------
+});
