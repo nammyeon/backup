4cd25827f6c40b129421c55a7dc0a346e71600a8
nammyeon <91935298+nammyeon@users.noreply.github.com>
2021-11-04 14:53:49 +0900

    Add files via upload

--- doc/basic05.js
+++ doc/basic05.js
@@ -13,5 +13,69 @@ $('.main_slider').slick({
     pauseOnFocus:false,
     pauseOnHover:false,
 });
+
+
+
+$('.pic').slick({
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:2000,    
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    asNavFor:'.pic',
+});
+
+
+
+
+
+
+$('.pr03_slider').slick({
+    arrows:false,
+    slidesToShow:5,
+    autoplay:true,
+    autoplaySpeed:2000,   
+    pauseOnFocus:false,
+    pauseOnHover:false,
+
+    // centerMode:true,
+    // centerPadding:'100px',
+});
+
+$('.pr03_slider figure').eq(7).addClass('on');
+$('.pr03_slider').on('afterChange', function(e,s,c){
+    // console.log(c)
+    // c= 0, 1, 2 ....
+    $('.pr03_slider figure').eq(c+7).addClass('on').siblings().removeClass('on');
+    $('#product03 .slide_bar>span').css({left:c*100})
+});
+
+
+
+$('.pr04_slider').slick({
+    arrows:false,
+    slidesToShow:3,
+    autoplay:true,
+    autoplaySpeed:2000,   
+    pauseOnFocus:false,
+    pauseOnHover:false,
+});
+
+
+$('#product04 .xi-arrow-left').on('click', function(){
+    $('.pr04_slider').slick('slickPrev');
+});
+$('#product04 .xi-arrow-right').on('click', function(){
+    $('.pr04_slider').slick('slickNext');
+});
+
+
+
+$('#link select').on('change', function (){
+    var link=$(this).val();
+    if(link) window.open(link);
+});
+
+
 //---------------------------------
 });
