2720151b4e4381644ec439ff744fbea69dad711c
nammyeon <91935298+nammyeon@users.noreply.github.com>
2021-12-07 10:34:50 +0900

    Add files via upload

--- doc/vertical02.js
+++ doc/vertical02.js
@@ -12,6 +12,26 @@ $(function(){
     $('.main_visual').on('afterChange', function(e,s,c){
         $('.main_visual figure').eq(c+1).addClass('on').siblings().removeClass('on');
 
-    })
+    });
 
+    $('.mopen').on('click', function(){
+        $('#header').toggleClass('on');
+    });
+
+
+    $('nav>ul>li>a').on('click', function(){
+        if ($('#header').hasClass('on')){
+            $(this).next().slideToggle();
+            $(this).parent().siblings().find('.submenu').slideUp();
+        };
+    });
+
+    $(window).on('resize', function(){
+        if($(window).width() > 768){
+            $('#header').removeClass('on');
+            $('.submenu').removeAttr('style');
+        };
+    });
+
+    
 });
