263856fa4d4dc25a9250e56e350141ef6fce96f1
nammyeon <91935298+nammyeon@users.noreply.github.com>
2021-12-17 10:44:16 +0900

    Add files via upload

---
+++ doc/scrollevent01.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    let con02Top = $('.con02').offset().top;
+    $(window).on('scroll', function(){
+        let sct=$(window).scrollTop();
+        console.log(sct,con02Top);
+        if(sct > con02Top) {
+            $('.con02').addClass('on')
+        }else{
+            $('.con02').removeClass('on')
+        }
+    });
+
+
+    
+    // $(window).on('scroll', function(){
+    //     let sct=$(window).scrollTop();
+    //     $('.ani').each(function(index,item){
+    //         let secTop = $(this).offset().top;
+    //         sct > secTop - 300 ? $(this).addClass('on') : $(this).removeClass('on');
+            
+    //     })
+    // })
+});
