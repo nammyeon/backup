c39f93a4dde30d6601e81d4b05548bbd119a3710
nammyeon <91935298+nammyeon@users.noreply.github.com>
2022-01-17 16:35:35 +0900

    Add files via upload

--- pf05/js/nam.js
+++ pf05/js/nam.js
@@ -129,12 +129,19 @@ $(function(){
 
 
     $('.gnb>ul>li>a').on('click', function(){
-        $(this).next().stop().slideToggle();
-        $(this).parent().siblings().find('.d_nav').slideUp();
+        if($(window).width()<769) {
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.d_nav').slideUp();
+        };
+
     });
+
     $('.d_nav>li>a').on('click', function(){
-        $(this).next().stop().slideToggle();
-        $(this).parent().siblings().find('.dd_nav').slideUp();
+        if($(window).width()<769) {
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.dd_nav').slideUp();
+        };
+
     });
 
 
